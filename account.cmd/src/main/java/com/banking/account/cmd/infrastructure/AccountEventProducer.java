b4e35ef42593caa9883c06cf520fd753b51641c3
ROBERTHP <ROBERTHP roberthpinchao@gmail.com>
2022-03-11 10:17:06 -0500

    Interfaces cqrs.core e Implementacion de kafka Producers cmd.infraestructure

---
+++ account.cmd/src/main/java/com/banking/account/cmd/infrastructure/AccountEventProducer.java
@@ -0,0 +1,23 @@
+package com.banking.account.cmd.infrastructure;
+
+import com.banking.cqrs.common.events.BaseEvent;
+import com.banking.cqrs.common.producers.EventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class AccountEventProducer implements EventProducer {
+
+    //inicializar un template kafka inyectar
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+    /**
+     * enviar mensaje hacia el topic
+     * @param topic canal de comunicaciòn
+     * @param event
+     */
+    @Override
+    public void producer(String topic, BaseEvent event) {
+        this.kafkaTemplate.send(topic, event);
+
+    }
+}
